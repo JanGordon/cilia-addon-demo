1157fe2f4535bc62a371ab458ac580413a679635
Jan <timmyjan46@gmail.com>
2023-01-26 19:16:16 +0000

    markdown modifier

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
+import "github.com/gomarkdown/markdown"
+
 // This calls a JS function from Go.
-func main() {
-	println("adding two numbers:") // expecting 5
-}
+func main() {}
 
 // ...omitted
 
 // This function is exported to JavaScript, so can be called using
 // exports.multiply() in JavaScript.
 //
-//export multiply
-func multiply(x, y int) int {
-	return x * y
+//export Modifier
+func Modifier(content string) string {
+	md := []byte(content)
+	output := markdown.ToHTML(md, nil, nil)
+	return string(output)
+
 }
