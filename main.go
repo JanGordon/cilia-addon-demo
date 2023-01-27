158ae89d31674b25002a9372904f258100b8e972
Jan <timmyjan46@gmail.com>
2023-01-27 22:26:00 +0000

    simplify

--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"bytes"
-	"fmt"
-
-	"github.com/yuin/goldmark"
-)
-
 // This calls a JS function from Go.
 func main() {}
 
@@ -16,14 +9,15 @@ func main() {}
 // exports.multiply() in JavaScript.
 //
 //export Modifier
-func Modifier(content string) string {
-	md := []byte(content)
-	var buf bytes.Buffer
-	if err := goldmark.Convert(md, &buf); err != nil {
-		panic(err)
-	}
-	println(content)
-	fmt.Println("golang")
-	return "from golang!!"
+func Modifier(message string) {
+	// md := []byte()
+	// var buf bytes.Buffer
+	// if err := goldmark.Convert(md, &buf); err != nil {
+	// 	panic(err)
+	// }
+	// println(content)
+	// fmt.Println("golang")
+	// return 901459
+	println(message + "golang")
 
 }
